abf03fd6df47fa9e3838a637d336e0c7d7c414ab
Liyabona Mxhalisa <mxhalisalm@gmail.com>
2023-06-02 16:24:38 +0200

    Fourth Commit

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
-// USING THE  DOM
+//Using the Dom Method
+
 const newTaskInput = document.getElementById('newTaskInput');
 const addTaskButton = document.getElementById('addTaskButton');
 const sortButton = document.getElementById('sortButton');
 const taskList = document.getElementById('tasks');
 
-// Creating  an empty array to store the tasks
+// Creating an empty array to store the tasks
 let tasks = [];
 
-// Creating a Function to render the tasks in the task list
+// Creating a function to render the tasks in the task list
 function renderTasks() {
-  
-  
-  
-    // Clear the task list
+  // Clear the task list
   taskList.innerHTML = '';
 
-  // Looping  through the tasks array
+  // Looping through the tasks array
   for (let i = 0; i < tasks.length; i++) {
     const task = tasks[i];
 
     // Creating a new list item for each task
     const listItem = document.createElement('li');
 
-    // Creating  a checkbox for the task
+    // Creating a checkbox for the task
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    
-    
+    checkbox.checked = task.completed; // Set checkbox state based on task completion
     checkbox.addEventListener('change', function () {
-
+      
       // Toggling the completed state of the task
-
       task.completed = !task.completed;
-      renderTasks(); // Rendering  the tasks to update the UI(user's interaction )
+      renderTasks(); // Re-rendering  the tasks to update the UI (User Interaction)
     });
 
-    // Creating  a span element for the task name
-
+    // Creating a span element for the task name
 
     const taskName = document.createElement('span');
     taskName.innerText = task.name;
 
-    // Adding  the necessary classes based on the completed state
+
+    // Adding the necessary classes based on the completed state
+
     if (task.completed) {
       listItem.classList.add('completed');
     }
 
-    // Creating  a button to remove the task
+   
+    // Creating a button to remove the task
     const closeButton = document.createElement('span');
     closeButton.classList.add('close-btn');
     closeButton.innerHTML = '&times;';
     closeButton.addEventListener('click', function () {
-      // Remove the task from the array
+      // Removing  the task from the array
       tasks.splice(i, 1);
-      renderTasks(); // Render the tasks to update the UI
+      renderTasks(); // Re-render the tasks to update the UI
     });
 
-    // Append the elements to the list item
+    
+    // Appending  the elements to the list item
     listItem.appendChild(checkbox);
     listItem.appendChild(taskName);
     listItem.appendChild(closeButton);
@@ -70,13 +69,13 @@ function renderTasks() {
 function addTask() {
   const taskName = newTaskInput.value.trim();
 
-  // Validate the task name
+  // Validating the task name
   if (taskName === '' || taskName.length < 4) {
     alert('Please enter a valid task name (at least 4 characters).');
     return;
   }
 
-  // Creating a new task object
+  // Creating  a new task object
   const newTask = {
     id: tasks.length + 1,
     name: taskName,
@@ -94,7 +93,7 @@ function addTask() {
   renderTasks();
 }
 
-// Creating a Function to sort the tasks alphabetically
+// Function to sort the tasks alphabetically
 function sortTasks() {
   tasks.sort((a, b) => a.name.localeCompare(b.name));
   renderTasks(); // Render the sorted tasks
@@ -106,3 +105,5 @@ addTaskButton.addEventListener('click', addTask);
 // Event listener for the sort button
 sortButton.addEventListener('click', sortTasks);
 
+// Render the initial tasks
+renderTasks();
